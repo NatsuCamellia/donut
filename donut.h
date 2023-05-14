d48e64660ee7300d010aa323350c21ee8202e6a5
NatsuCamellia <natsucamellia@gmail.com>
2023-05-14 16:29:52 +0800

    get_projection + get_luminance

--- donut.h
+++ donut.h
@@ -1,24 +1,28 @@
-// donut.h
 #ifndef DONUT_H
 #define DONUT_H
-
+ 
 #define width 30
 #define height 30
-
+ 
 const float pi = 3.1415926 ;
 const float dtheta = 0.07 ;
 const float dphi   = 0.02 ;
-
+ 
 const float omega_A = 0.07;
 const float omega_B = 0.04;
-
+ 
+const float R1 = 1 ;
+const float R2 = 2 ;
+const float donut_distance = 5 ;
+const float render_distance = width*donut_distance*3/(8*(R1+R2));
+ 
 typedef struct Tcoordinate {
     int x ;
     int y ; 
     float z ;
 } coordinate ;
-
+ 
 char output[height][width] ;
 float bestOOZ[height][width] ;
-
+ 
 #endif
--- function.h
+++ function.h
@@ -1,15 +1,14 @@
-// function.h
 #ifndef FUNCTION_H
 #define FUNCTION_H
-
+ 
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-
+ 
 void clear_screen() {
     printf("\x1b[2J");
 }
-
+ 
 void print_screen(float A, float B) {
     printf("print_screen %f %f\n", A, B);
     for (int j = 0; j < height; j++) {
@@ -18,29 +17,50 @@ void print_screen(float A, float B) {
         putchar('\n');
     }
 }
-
+ 
 void hide_cursor() {
     printf("\033[?25l") ;
 }
-
+ 
 void reset_cursor() {
     printf("\x1b[H");
 }
-
+ 
 void reset_values() {
     memset(output, (int)' ', sizeof(output)) ;
     memset(bestOOZ, 0, sizeof(bestOOZ)) ;
 }
-
+ 
 coordinate get_projection(float theta, float phi, float A, float B) {
-    return (coordinate){ .x=0, .y=0, .z=0 };
+    float costheta = cos(theta), sintheta = sin(theta);
+    float cosphi = cos(phi), sinphi = sin(phi);
+    float cosA = cos(A), sinA = sin(A) ;
+    float cosB = cos(B), sinB = sin(B) ;
+ 
+    // the x,y coordinate of the circle before revolving
+    float circlex = R2 + R1*costheta;
+    float circley = R1*sintheta;
+ 
+    // final 3D (x,y,z) coordinate after rotations 
+    coordinate pos ;
+    float x = circlex*(cosB*cosphi + sinA*sinB*sinphi) - circley*cosA*sinB ; 
+    float y = circlex*(sinB*cosphi - sinA*cosB*sinphi) + circley*cosA*cosB ;
+    float z = donut_distance + cosA*circlex*sinphi + circley*sinA ;
+ 
+    // (x,y,z) after projection
+    pos.x = (int) (width/2 + render_distance*x/z);
+    pos.y = (int) (height/2 - render_distance*y/z);
+    pos.z = z ;
+ 
+    return pos ;
 }
-
+ 
 float get_luminance(float theta, float phi, float A, float B) {
-    return 0;
+    // calculate luminance, range from -sqrt(2) to +sqrt(2). 
+    return cos(phi)*cos(theta)*sin(B) - cos(A)*cos(theta)*sin(phi) - sin(A)*sin(theta) + cos(B)*(cos(A)*sin(theta) - cos(theta)*sin(A)*sin(phi));
 }
-
+ 
 void get_output(float L, coordinate pos) {
 }
-
+ 
 #endif
